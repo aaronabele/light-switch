8375f18fecc25d5638639a02ea6cb19b2f5f5a23
Aaron Abele <aaronabele.coding@gmail.com>
2022-12-23 16:15:04 +0100

    adds the code for a light switch

---
+++ script.js
@@ -0,0 +1,16 @@
+let dark = false;
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function (e) {
+  dark = !dark;
+  if (dark) {
+    document.body.classList.add("body-dark");
+    btn.classList.add("btn-off");
+    document.title = "Good Night";
+  } else {
+    document.body.classList.remove("body-dark");
+    btn.classList.remove("btn-off");
+    document.title = "Good Morning";
+  }
+});
